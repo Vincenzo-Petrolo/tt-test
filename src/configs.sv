8071583d9733afae4fda5f3426c8183206ff0c4f
Petrolo Vincenzo <vincenzo.petrolo99@gmail.com>
2024-05-17 15:02:48 +0200

    initial commit

---
+++ src/configs.sv
@@ -0,0 +1,15 @@
+`ifndef __DEC_CONFIGS_SV
+`define __DEC_CONFIGS_SV
+
+
+//  Package: ncu_configs
+//
+package configs;
+
+  //  Group: Parameters
+  localparam unsigned LiftingFactor = 81;
+  localparam unsigned DWIDTH = 5;
+
+endpackage : configs
+
+`endif
--- src/project.v
+++ src/project.v
@@ -1,24 +1,39 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Vincenzo Petrolo
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+`ifndef __QSN_SV
+`define __QSN_SV
+
+
+`include "configs.sv"
+
+module qsn
+  /*  package imports  */
+  import configs::*;
+(
+    input logic [DWIDTH-1:0] I[LiftingFactor],
+    logic [SEL-1:0] shift,
+    output logic [DWIDTH-1:0] O[LiftingFactor]
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  genvar i;
+  logic [DWIDTH-1:0] tmp[2*LiftingFactor];
 
-endmodule
+  generate;
+    for (i=0; i<LiftingFactor; i++) begin : gen_replicate
+      assign tmp[i] = I[i];
+      assign tmp[i+LiftingFactor] = I[i];
+    end
+
+    for (i=0; i<LiftingFactor; i++) begin : gen_assignOutput
+      assign O[i] = tmp[i+shift];
+    end
+  endgenerate
+
+endmodule : qsn
+
+`endif
