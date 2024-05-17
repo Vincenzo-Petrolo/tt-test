d4fa3208e273d1f50dab2f4ea022622b23ca0f34
Petrolo Vincenzo <vincenzo.petrolo99@gmail.com>
2024-05-17 18:17:00 +0200

    chore : ports conforming to pinout

--- src/qsn.v
+++ src/qsn.v
@@ -3,17 +3,26 @@ module tt_um_qsn #(
     localparam LiftingFactor = 4,
     localparam ShiftWidth = 2
 ) (
-    input wire [LiftingFactor - 1:0] I,
-    input wire [ShiftWidth - 1:0] shift,
-    output wire [LiftingFactor - 1:0] O,
-    input wire ena,  // always 1 when the design is powered, so you can ignore it
-    input wire clk,  // clock
-    input wire rst_n  // reset_n - low to reset
+	input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
   genvar i;
-  localparam configs_LiftingFactor = 4;
   wire tmp[0:7];
+
+  wire [LiftingFactor-1:0] I;
+  wire [LiftingFactor-1:0] O;
+  wire [ShiftWidth-1:0] shift;
+
+  assign I = {ui_in[0], ui_in[1], ui_in[2], ui_in[3]};
+  assign shift = {ui_in[4], ui_in[5]};
+  assign uo_out = {O[0], O[1], O[2], O[3]};
 
   generate
     for (i = 0; i < configs_LiftingFactor; i = i + 1) begin : gen_replicate
