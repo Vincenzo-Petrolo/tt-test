ccc76ea5d22264c52c45d09751941795e117a1dd
Petrolo Vincenzo <vincenzo.petrolo99@gmail.com>
2024-05-17 15:40:55 +0200

    fix : yosys bbs

--- src/qsn.sv
+++ src/qsn.sv
@@ -15,21 +15,23 @@ module tt_um_qsn
   /*  package imports  */
   import configs::*;
 (
-    input logic I[LiftingFactor],
+    input logic [LiftingFactor-1:0] I,
+
     logic [ShiftWidth-1:0] shift,
-    output logic O[LiftingFactor]
+    output logic [LiftingFactor-1:0] O
 );
 
   genvar i;
   logic tmp[2*LiftingFactor];
 
-  generate;
-    for (i=0; i<LiftingFactor; i++) begin : gen_replicate
+  generate
+    ;
+    for (i = 0; i < LiftingFactor; i++) begin : gen_replicate
       assign tmp[i] = I[i];
       assign tmp[i+LiftingFactor] = I[i];
     end
 
-    for (i=0; i<LiftingFactor; i++) begin : gen_assignOutput
+    for (i = 0; i < LiftingFactor; i++) begin : gen_assignOutput
       assign O[i] = tmp[i+shift];
     end
   endgenerate
