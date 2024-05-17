d75094a043f5d35ff832753f188c8fa4d6e3783b
Petrolo Vincenzo <vincenzo.petrolo99@gmail.com>
2024-05-17 15:58:57 +0200

    chore : localparams updates

--- src/qsn.v
+++ src/qsn.v
@@ -1,22 +1,25 @@
-`default_nettype none
-module tt_um_qsn (
-	I,
-	shift,
-	O
+// `default_nettype none
+module tt_um_qsn #(
+    localparam LiftingFactor = 4,
+    localparam ShiftWidth = 2
+) (
+    I,
+    shift,
+    O
 );
-	input wire [LiftingFactor - 1:0] I;
-	input wire [ShiftWidth - 1:0] shift;
-	output wire [LiftingFactor - 1:0] O;
-	genvar i;
-	localparam configs_LiftingFactor = 4;
-	wire tmp [0:7];
-	generate
-		for (i = 0; i < configs_LiftingFactor; i = i + 1) begin : gen_replicate
-			assign tmp[i] = I[i];
-			assign tmp[i + configs_LiftingFactor] = I[i];
-		end
-		for (i = 0; i < configs_LiftingFactor; i = i + 1) begin : gen_assignOutput
-			assign O[i] = tmp[i + shift];
-		end
-	endgenerate
+  input wire [LiftingFactor - 1:0] I;
+  input wire [ShiftWidth - 1:0] shift;
+  output wire [LiftingFactor - 1:0] O;
+  genvar i;
+  localparam configs_LiftingFactor = 4;
+  wire tmp[0:7];
+  generate
+    for (i = 0; i < configs_LiftingFactor; i = i + 1) begin : gen_replicate
+      assign tmp[i] = I[i];
+      assign tmp[i+configs_LiftingFactor] = I[i];
+    end
+    for (i = 0; i < configs_LiftingFactor; i = i + 1) begin : gen_assignOutput
+      assign O[i] = tmp[i+shift];
+    end
+  endgenerate
 endmodule
